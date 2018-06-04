2d31cf4d765ed489782fb0aa0b70296087441d56
Shun Yao <ysmiracle@hotmail.com>
2018-06-04 03:12:59 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,18 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+export default Bomb extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    return (
+      <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+    )
+  }
+};
